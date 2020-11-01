0382fc97711aef08d3f0ec1a497526d6780c86cb
Ian Mandarini <ian@ianmandarini.com>
2020-11-01 18:34:33 -0300

    props

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -9,43 +9,47 @@ public class DialogueTrigger : MonoBehaviour
     private DialogueManager _dialogueManager;
     private bool _hasStartedDialogue = false;
     private bool _isInRange = false;
+    
+    public GameObject interactionIndicator = default;
 
     private void Start()
     {
-        _dialogueManager = FindObjectOfType<DialogueManager>();
+        this._dialogueManager = FindObjectOfType<DialogueManager>();
+        this.interactionIndicator.SetActive(false);
     }
 
     private void Update()
     {
-        if (_isInRange && Input.GetButtonDown("Fire2"))
+        if (this._isInRange && Input.GetButtonDown("Fire2"))
         {
-            if (_hasStartedDialogue == false)
+            if (this._hasStartedDialogue == false)
             {
-                _dialogueManager.StartDialogue(dialogue);
-                _hasStartedDialogue = true;
+                this._dialogueManager.StartDialogue(this.dialogue);
+                this._hasStartedDialogue = true;
             }
             else
             {
-                _dialogueManager.DisplayNextSentence();
+                this._dialogueManager.DisplayNextSentence();
             }
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _isInRange = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag == "Player")
-        {
-            animator.SetBool("IsOpen", false);
-            _hasStartedDialogue = false;
-            _isInRange = false;
-        }
+        if (!other.CompareTag("Player"))
+            return;
+        this._isInRange = false;
+        this.interactionIndicator.SetActive(false);
+        this.animator.SetBool("IsOpen", false);
+        this._hasStartedDialogue = false;
+        this._isInRange = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+        this._isInRange = true;
+        this.interactionIndicator.SetActive(true);
     }
 }
